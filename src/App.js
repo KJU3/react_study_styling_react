3be423ba9c47d498cd896aad704e0813d836a333
KJU3 <kju_3@naver.com>
2024-09-30 20:54:33 +0900

    ch9 complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import SassComponent from './SassComponent';
+import StyledComponent from './StyledComponent';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <SassComponent/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <StyledComponent />
+            </div>
+        );
+    }
 }
 
 export default App;
